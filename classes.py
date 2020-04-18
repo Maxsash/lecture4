179b726df6828a867f3f66fbdc7d8990d0636d85
Yash <the.revived.1@gmail.com>
2020-04-18 21:39:26 +0530

    Added and used a print method

--- classes.py
+++ classes.py
@@ -4,6 +4,11 @@ class Flight:
         self.origin = origin
         self.destination = destination
         self.duration = duration
+    
+    def print_info(self):
+        print(f"Flight origin: {self.origin}")
+        print(f"Flight destination: {self.destination}")
+        print(f"Flight duration: {self.duration}")
 
 def main():
     
@@ -17,6 +22,10 @@ def main():
     print(f.origin)
     print(f.destination)
     print(f.duration)
+
+    # create another flight
+    f2 = Flight(origin="Tokyo", destination="Shanghai", duration=185)
+    f2.print_info()
 
 if __name__ == "__main__":
     main()
