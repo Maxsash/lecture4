d0210b2ab42d36ee895d4e02e0d690dc83c1d280
Yash <the.revived.1@gmail.com>
2020-04-18 21:24:07 +0530

    Initialize a class

---
+++ classes.py
@@ -0,0 +1,6 @@
+class Flight:
+    ## special inbuilt method. Take these inputs and make their references.
+    def __init__(self, origin, destination, duration):
+        self.origin = origin
+        self.destination = destination
+        self.duration = duration
