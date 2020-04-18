23c48ffefb1e65639f9e0a67e48138bacb848330
Yash <the.revived.1@gmail.com>
2020-04-18 21:27:26 +0530

    Basic usage of class

--- classes.py
+++ classes.py
@@ -4,3 +4,19 @@ class Flight:
         self.origin = origin
         self.destination = destination
         self.duration = duration
+
+def main():
+    
+    # create flight.
+    f = Flight(origin="New York", destination="Paris", duration=540)
+
+    #change the value of a variable.
+    f.duration += 10
+
+    # print details about flight.
+    print(f.origin)
+    print(f.destination)
+    print(f.duration)
+
+if __name__ == "__main__":
+    main()
