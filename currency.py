764742bff26994b5c4785578275198a37622c9c1
Yash <the.revived.1@gmail.com>
2020-04-19 17:50:19 +0530

    Basic API interaction

---
+++ currency.py
@@ -0,0 +1,17 @@
+import requests
+
+def main():
+    #defining parameters to reduce length of line
+    base_url = "http://data.fixer.io/api/latest?access_key="
+    access_key = 'd84f163bd5d22505eca254b51dc1d5ff'
+
+    res = requests.get(f"{base_url}{access_key}&base=EUR&symbols=EUR")
+    if res.status_code != 200:
+        raise Exception("ERROR: API request unsuccessful.")
+    
+    #read the json data and print
+    data = res.json()
+    print(data)
+
+if __name__ == "__main__":
+    main()
