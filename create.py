3aa35c5dbbcf4502a76853f4b1bf244441148c4a
Yash <the.revived.1@gmail.com>
2020-04-19 15:34:47 +0530

    using SELECT with slqalchemy

--- create.py
+++ create.py
@@ -14,6 +14,7 @@ def main():
     #creates the databases in accordance with models.py
     db.create_all()
 
+    #demonstrating INSERT
     f=open("flights.csv")
     reader = csv.reader(f)
     for origin, destination, duration in reader:
@@ -22,6 +23,11 @@ def main():
         db.session.add(flight)
         print(f"Added flight from {origin} to {destination} lasting {duration} minutes.")
     db.session.commit()
+
+    #demonstrating SELECT
+    flights = Flight.query.all()
+    for flight in flights:
+        print(f"{flight.origin} to {flight.destination}, {flight.duration} minutes.")
 
 if __name__ == "__main__":
     with app.app_context():
