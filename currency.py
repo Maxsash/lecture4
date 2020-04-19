4c332804baa7aed87a921b661127313f8c093843
Yash <the.revived.1@gmail.com>
2020-04-19 18:03:21 +0530

    Improve parameter specification

--- currency.py
+++ currency.py
@@ -2,10 +2,14 @@ import requests
 
 def main():
     #defining parameters to reduce length of line
-    base_url = "http://data.fixer.io/api/latest?access_key="
+    base_url = "http://data.fixer.io/api/latest"
     access_key = 'd84f163bd5d22505eca254b51dc1d5ff'
 
-    res = requests.get(f"{base_url}{access_key}&base=EUR&symbols=EUR")
+    base = "EUR"
+    other = "EUR"
+
+    res = requests.get(f"{base_url}",
+                        params={"access_key": access_key, "base": base, "symbols": other})
     if res.status_code != 200:
         raise Exception("ERROR: API request unsuccessful.")
     
